b936cebc992a57e1ce3e7ef550107f23c9341af3
Yarik <yarik.mihalko@gmail.com>
2017-04-27 17:36:24 +0300

    Fix menus

--- GameChessClient/Assets/Scripts/RegistrationController.cs
+++ GameChessClient/Assets/Scripts/RegistrationController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RegistrationController : MonoBehaviour {
 
-	
-	void Start () {
+    public InputField mc_login;
+    public InputField mc_name;
+    public InputField mc_email;
+    public InputField mc_password;
+    public InputField mc_returnPassword;
+
+    void Start () {
 		
 	}
 	
@@ -15,8 +21,23 @@ public class RegistrationController : MonoBehaviour {
 	}
 
 
-    public void Registration()
+    public void f_Registration()
     {
-        Main.main.Test();
+        //Main.main.Test();
+
+        if (mc_login.text.Length > 0 && mc_name.text.Length > 0 && mc_email.text.Length > 0 &&
+           mc_password.text.Length > 0 && mc_returnPassword.text.Length > 0)
+        {
+            print(22);
+
+
+
+        }
+
+        else print("error");
+
     }
+
+
+
 }
